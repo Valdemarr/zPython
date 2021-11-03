0df592f99fcbe7f95b96c5d0255c414fce1e3f52
Valdemarr <valdemarsthoisen@gmail.com>
2021-11-03 21:08:28 +0100

    init

--- Projects/NFT shenanigans/NFT Shenanigans.py
+++ Projects/NFT shenanigans/NFT Shenanigans.py
@@ -1,5 +1,25 @@
-import selenium
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 # Using Chrome to access web
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+url = "https://tinkersynth.com/slopes/"
+
 # Open the website
-driver.get('https://tinkersynth.com/slopes/')
+driver.get(url)
+
+time.sleep(7)
+
+# Select the id box
+id_box = driver.find_element_by_class_name(
+    "UnstyledButton-lw1m82-0 Button__Wrapper-ikf7gj-0 Button__LargeButton-ikf7gj-2 hJLKbV")
+
+time.sleep(7)
+
+id_box.click()
+
+time.sleep(7)
+
+png = driver.find_element_by_class_name("DownloadVariant__Overlay-sc-3v1shs-2 cnnBWt")
+png.click()
