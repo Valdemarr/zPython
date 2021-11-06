61a13a1838a0962a13c69e5a0b1f98d2ebcd3c87
Valdemarr <valdemarsthoisen@gmail.com>
2021-11-06 12:17:43 +0100

    Imported working morse version from PG ECHO

--- Playgrounds/PG ALPHA.py
+++ Playgrounds/PG ALPHA.py
@@ -1,52 +1,49 @@
-def decodeMorse(morse_code):
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
-    return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
+list = ["A", ".-",     "B", "-...",   "C", "-.-.",
+        "D", "-..",    "E", ".",      "F", "..-.",
+        "G", "--.",    "H", "....",   "I", "..",
+        "J", ".---",   "K", "-.-",    "L", ".-..",
+        "M", "--",     "N", "-.",     "O", "---",
+        "P", ".--.",   "Q", "--.-",   "R", ".-.",
+        "S", "...",    "T", "-",      "U", "..-",
+        "V", "...-",   "W", ".--",    "X", "-..-",
+        "Y", "-.--",   "Z", "--..",
+
+        "0", "-----",  "1", ".----",  "2", "..---",
+        "3", "...--",  "4", "....-",  "5", ".....",
+        "6", "-....",  "7", "--...",  "8", "---..",
+        "9", "----.", " ", ""]
 
 
-def morse_decoder(text):
-    morse = ["A", ".-",     "B", "-...",   "C", "-.-.",
-             "D", "-..",    "E", ".",      "F", "..-.",
-             "G", "--.",    "H", "....",   "I", "..",
-             "J", ".---",   "K", "-.-",    "L", ".-..",
-             "M", "--",     "N", "-.",     "O", "---",
-             "P", ".--.",   "Q", "--.-",   "R", ".-.",
-             "S", "...",    "T", "-",      "U", "..-",
-             "V", "...-",   "W", ".--",    "X", "-..-",
-             "Y", "-.--",   "Z", "--..",
-
-             "0", "-----",  "1", ".----",  "2", "..---",
-             "3", "...--",  "4", "....-",  "5", ".....",
-             "6", "-....",  "7", "--...",  "8", "---..",
-             "9", "----."]
-
-    # Making the function parameter a list
-    text = text.split()
-
+def morse(text):
+    index_list = []
+    conv_index_list = []
     result = ""
-    list_of_indices = []
 
-    for i in text:
-        if i in morse:
-            list_of_indices.append(int(morse.index(i)))
+    text2list = text.replace("   ", "  ").split(" ")
+    print(text)
+    print(text2list)
 
-#    for i in list_of_indices:
-#        print(f"2nd for: {morse[list_of_indices[i]]}")
+#    for letter in text:
+    # Put the letters into a list --> ["v", "a", "l"]
+#        text2list.append(letter)
 
-    list_of_indices_0 = list_of_indices[0]
-    decode = morse[list_of_indices_0 - 1]
+    for element in text2list:
+        # Find the index of each element from the list "text2list" in the list "list"
+        what_index = list.index(element)
+        # Put indices into list --> [1, 3, 5]
+        index_list.append(what_index)
 
-    print(f"index test: {morse[list_of_indices[0]-1]}")
-    print(f"decode: {decode}")
+    for element2 in index_list:
+        # remove -1 from each index element in the list "index_list" into new list "conv_index_list"
+        conv_index_list.append(element2-1)
 
-    print(f"text: {text}")
-    print(f"list_of_indices: {list_of_indices}")
-    print(result)
+    for loop in conv_index_list:
+        # Convert new index numbers back to letters
+        result += list[loop]
+
+    print(f"result: {result}")
 
 
-test = "--.. .--. -.-- - .... --- -."
+test = ".... . -.--   .--- ..- -.. ."
 
-morse_decoder(test)
-
-test = 2
-test2 = "yea"
-test3 = "oooh oh ohh yeeee"
+morse(test)
