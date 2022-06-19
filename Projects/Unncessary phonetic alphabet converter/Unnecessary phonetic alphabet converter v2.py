dd7e8ce46bfd8fa71cdc7d0c4d62d28a3b33c15d
Valdemarr <valdemarsthoisen@gmail.com>
2022-06-19 23:10:55 +0200

    tee

--- Projects/Unncessary phonetic alphabet converter/Unnecessary phonetic alphabet converter v2.py
+++ Projects/Unncessary phonetic alphabet converter/Unnecessary phonetic alphabet converter v2.py
@@ -1,6 +1,7 @@
-def un_phon_al_conv(text_to_convert: str):
+def unphalcon(text_to_convert: str, latinize:bool, print_it:bool):
     text_to_convert = text_to_convert.upper()
-    result = ""
+    alphacon = ""
+    latinized = ""
 
     alphabet = {"A": "Alpha",       "B": "Bravo",       "C": "Charlie",
                 "D": "Delta",       "E": "Echo",        "F": "Foxtrot",
@@ -14,18 +15,36 @@ def un_phon_al_conv(text_to_convert: str):
 
     for i in text_to_convert:
         if i in alphabet.keys():
-            result += alphabet[i]
-            result += " "
-        if i not in alphabet.keys():
-            result += i
-            result += " "
+            tempy = alphabet[i]
+            
+            alphacon += tempy + " "
+            latinized += tempy[0:2]
 
-    fhand = open(
-        r"G:\Mit drev\zPython\Projects\Unncessary phonetic alphabet converter\unphalconed.txt", "a")
-    fhand.write(f"New unphalcone: \n{result}\n\n")
-    fhand.close()
+        elif i not in alphabet.keys():
+            alphacon += i + " "
 
-    print(f"'{text_to_convert}' sucessfully written to unphalconed.txt")
-    return result
 
-print(un_phon_al_conv("valdemar"))
+    spacer = len(text_to_convert) //2 * 2
+    first_half = latinized[:spacer].lower().capitalize()
+    second_half = latinized[spacer:].lower().capitalize()
+
+    #print(first_half + " " + second_half)
+    latinized = first_half + " " + second_half
+
+
+    if latinize == False:
+        result = alphacon
+    else:
+        result = latinized
+
+    #fhand = open(   r"G:\Mit drev\zPython\Projects\Unncessary phonetic alphabet converter\unphalconed.txt", "a")
+    #fhand.write(f"New unphalcon, latinize is {latinize}: \n{result}\n\n")
+    #fhand.close()
+
+    #print(f"'{text_to_convert}' sucessfully written to unphalconed.txt")
+    if print_it == True:
+        print(result)
+    else:
+        return result
+
+unphalcon("valdemar", True, True)
