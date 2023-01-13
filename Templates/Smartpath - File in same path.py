b5af60eea769f9d3d902118a1aad4e805b91a149
Valdemarr <valdemarsthoisen@gmail.com>
2023-01-13 14:06:42 +0100

    Added a broken CHAD version

--- Templates/Smartpath - File in same path.py
+++ Templates/Smartpath - File in same path.py
@@ -9,6 +9,11 @@ f = open(os.path.join(__location__, 'input.txt'), "r") # Open and read input fil
 f.close()
 
 # Writing result to file in directory - GOOD FOR LOGGING
+#Beta version
 f2 = open(os.path.join(__location__, 'output.txt'), "w") #open and write to output file
 f2.write("whatever") #Write to output file
 f2.close()
+
+#CHAD version
+with open(os.path.join(__location__, "w")) as file:
+    file.write("hi, my name is chad")
